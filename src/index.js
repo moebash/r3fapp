764bd024cd1a1e9dd7b2dea3e890fa06bbca4fc1
moebash <moebashkhelo@gmail.com>
2022-10-09 01:58:45 +0400

    changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useState } from "react"
+import ReactDOM from "react-dom"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./App.css"
+import App from "./App"
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Overlay() {
+  const [ready, set] = useState(false)
+  return (
+    <>
+      <App />
+      <div className="dot" />
+      <div className={`fullscreen bg ${ready ? "ready" : "notready"} ${ready && "clicked"}`}>
+        <div className="stack">
+          <button onClick={() => set(true)}>Click (needs fullscreen)</button>
+        </div>
+       
+      </div>
+    </>
+  )
+}
+
+ReactDOM.render(<Overlay />, document.getElementById("root"))
